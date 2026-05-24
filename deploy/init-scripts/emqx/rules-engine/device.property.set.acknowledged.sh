290ea1dde11a2cfb654cdc968368bc50e6c5b273
ldchengyi <krzcheng257@gmail.com>
2026-05-24 14:18:58 +0000

    feat: add property set ack, fix emqx rule bugs

--- deploy/init-scripts/emqx/rules-engine/device.property.set.acknowledged.sh
+++ deploy/init-scripts/emqx/rules-engine/device.property.set.acknowledged.sh
@@ -1,5 +1,8 @@
 # rule for: device.property.set.acknowledged
 # topic: lf/v1/{tenant_slug}/{product_key}/{device_slug}/property/up/set_reply
+#
+# Device payload shape:
+#   {"command_id":"018f56d3-7cb7-7f1a-9b41-3f3a63fd3db8","success":true,"code":"ok","message":"applied","properties":{"led_on":true}}
 
 RULE_ID="lf-device-property-set-ack"
 RULE_NAME="device.property.set.acknowledged"
@@ -17,6 +20,7 @@ SELECT
   1 as event_version,
   format_date('millisecond', '+00:00', '%Y-%m-%dT%H:%M:%S.%3NZ', now_timestamp('millisecond')) as occurred_at,
   'emqx-rule-engine' as producer,
+  payload.command_id as causation_id,
   client_attrs.tenant_id as tenant_id,
   client_attrs.tenant_id as payload.tenant_id,
   client_attrs.product_id as payload.product_id,
